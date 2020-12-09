3ca4fa7489d9cba97d1e379c9d364b9e14af1e32
Siphenathi <spantshwa.lukho@gmail.com>
2020-12-09 14:27:18 +0200

    wip with image slider

--- script.js
+++ script.js
@@ -1,14 +1,19 @@
 let currentSlide = 0;
-const slides = document.querySelectorAll(".slide")
+const slides = document.querySelectorAll(".slide");
 const dots = document.querySelectorAll('.dot')
 
 const init = (n) => {
+  console.log("slides. :", slides)
   slides.forEach((slide, index) => {
+    console.log("inside loop :", slide)
     slide.style.display = "none"
     dots.forEach((dot, index) => {
       dot.classList.remove("active")
     })
   })
+
+  
+
   slides[n].style.display = "block"
   dots[n].classList.add("active")
 }
