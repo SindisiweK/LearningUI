ce6bc5370d5699f7bbe73cccbdca20ad74185139
Siphenathi <spantshwa.lukho@gmail.com>
2020-12-10 06:42:02 +0200

    img slide in progress

--- script.js
+++ script.js
@@ -1,47 +1,12 @@
-let currentSlide = 0;
-const slides = document.querySelectorAll(".slide");
-const dots = document.querySelectorAll('.dot')
+const slideContainer = document.querySelector('.slide-fade')
+const slideImages = document.querySelectorAll('.slide-fade img')
 
-const init = (n) => {
-  console.log("slides. :", slides)
-  slides.forEach((slide, index) => {
-    console.log("inside loop :", slide)
-    slide.style.display = "none"
-    dots.forEach((dot, index) => {
-      dot.classList.remove("active")
-    })
-  })
+const prevBtn = document.querySelector('#btnprev')
+const nextBtn = document.querySelector('#btnnext')
 
-  
-
-  slides[n].style.display = "block"
-  dots[n].classList.add("active")
-}
-document.addEventListener("DOMContentLoaded", init(currentSlide))
-const next = () => {
-  currentSlide >= slides.length - 1 ? currentSlide = 0 : currentSlide++
-  init(currentSlide)
-}
-
-const prev = () => {
-  currentSlide <= 0 ? currentSlide = slides.length - 1 : currentSlide--
-  init(currentSlide)
-}
-
-document.querySelector(".next").addEventListener('click', next)
-
-document.querySelector(".prev").addEventListener('click', prev)
+let counter = 1;
+const size = slideImages[0].clientWidth;
 
 
-setInterval(() => {
-  next()
-}, 5000);
 
-dots.forEach((dot, i) => {
-  dot.addEventListener("click", () => {
-    console.log(currentSlide)
-    init(i)
-    currentSlide = i
-  })
-})
 
